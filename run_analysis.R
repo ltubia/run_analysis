c7c0238347df87f49c7c5e52814e2ad1331c9001
ltubia <leandro.tubia@gmail.com>
2015-07-21 19:29:08 +0000

    analysis_run second version

--- run_analysis.R
+++ run_analysis.R
@@ -21,28 +21,28 @@
 #       Parameters: a plain file name, desirable without extension.
 # 
 #       Logic:          
-#                       It executes the course project requirements, not necesarily in the same order (explained in more detail in Note 2 below)
+#               It executes the course project requirements, not necesarily in the same order (explained in more detail in Note 2 below)
 #
-#                               Req 1. Merges the training and the test sets to create one data set.
-#                               Req 2. Extracts only the measurements on the mean and standard deviation for each measurement. 
-#                               Req 3. Uses descriptive activity names to name the activities in the data set
-#                               Req 4. Appropriately labels the data set with descriptive variable names. 
-#                               Req 5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+#                       Req 1. Merges the training and the test sets to create one data set.
+#                       Req 2. Extracts only the measurements on the mean and standard deviation for each measurement. 
+#                       Req 3. Uses descriptive activity names to name the activities in the data set
+#                       Req 4. Appropriately labels the data set with descriptive variable names. 
+#                       Req 5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
 #
-#                       Note 1: It is not needed to download zip file as this function does it by itself and store it in a temp file
+#               Note 1: It is not needed to download zip file as this function does it by itself and store it in a temp file
 #
-#                       Note 2: for the sake of simplicity and optimization, not all steps are executed in the above order. They are 
-#                               executed in the following one:
+#               Note 2: for the sake of simplicity and optimization, not all steps are executed in the above order. They are 
+#                executed in the following one:
 #
-#                               1. Download file
-#                               2. load common files for both train and test data: features and activiy labels
-#                               3. Load all test related files: x_test, y_test, subject_test
-#                               4. Merge them all in a unique test data table and assign labels and descriptive names to columns (Req 4 and Req 3)
-#                               5. Load all train related files: x_train, y_train, subject_train
-#                               6. Merge them all in a unique train data table and assign labels and descriptive names to columns (Req 4 and Req 3)
-#                               7. Merge train and test data (Req 1)
-#                               8. Extract mean and std measures (Req 2)
-#                               9. Calculate average of each variable for each activity and each subject and save it to disk (Req 5)
+#                       1. Download file
+#                       2. load common files for both train and test data: features and activiy labels
+#                       3. Load all test related files: x_test, y_test, subject_test
+#                       4. Merge them all in a unique test data table and assign labels and descriptive names to columns (Req 4 and Req 3)
+#                       5. Load all train related files: x_train, y_train, subject_train
+#                       6. Merge them all in a unique train data table and assign labels and descriptive names to columns (Req 4 and Req 3)
+#                       7. Merge train and test data (Req 1)
+#                       8. Extract mean and std measures (Req 2)
+#                       9. Calculate average of each variable for each activity and each subject and save it to disk (Req 5)
 #
 #
 
